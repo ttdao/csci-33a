e9ff52ec51e4c94c8e09dbdf9c13fe50a7b4f96e
Tien <thientien.dao@gmail.com>
2022-02-17 09:41:06 -0500

    Merging Changes

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -18,31 +18,6 @@ def entry(request, title):
             "entries": util.get_entry(title)
     })
 
-<<<<<<< HEAD
-# Class to create form instance
-#class SearchTerm(forms.Form):
-#   term = forms.CharField(label="Search Term")
-
-# Find entry in search bar. If query does not match, show search results 
-# Clicking an entry in the search results screen will take them to the page
-def search(request):
-    entries = util.list_entries()
-    
-    # If method is GET
-    if request.method == "GET":
-        search_term = request.GET.get('q')
-        # Go to page with search term
-        return render(request, "encyclopedia/entry.html", {
-            "entries": util.get_entry(search_term)
-            })
-
-    
-    #else:
-
-    # Take term as substring
-    # Compare to each entry in list of entries - look at python lesson
-    # Display all relevant entries in results.html 
-=======
 # Find entry in search bar. If query does not match, show search results 
 # Clicking an entry in the search results screen will take them to the page
 def search(request):
@@ -70,6 +45,5 @@ def search(request):
             return render(request, "encyclopedia/pagenotfound.html")
              #No page found placeholder
         # Display all relevant entries in results.html 
->>>>>>> origin/SearchBox
            
 
