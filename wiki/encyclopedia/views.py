9298971eacad5cff95ce7b95344ccb1ff9a02b55
Tien Dao <thientien.dao@gmail.com>
2022-02-16 23:13:14 -0500

    Search Box
    Modified views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -18,34 +18,32 @@ def entry(request, title):
             "entries": util.get_entry(title)
     })
 
-# Class to create form instance
-class SearchTerm(forms.Form)
-    term = forms.CharField(label="Search Term")
-
 # Find entry in search bar. If query does not match, show search results 
 # Clicking an entry in the search results screen will take them to the page
 def search(request):
-    entries = util.list_entries()
 
-    # If method is POST
-    if request.method == "POST":
+    # Lowercase all entries
+    entries = [entry.lower() for entry in util.list_entries()]
+    results = list()
+    term = request.GET.get('q')
 
-        # Take in client-side search term
-        term = SearchTerm(request.post)
+    # If term exist in list of entries, show page
+    if term in entries:
+            return render(request, "encyclopedia/entry.html", {
+        "entries": util.get_entry(term)
+        })
 
-        # Check if it exists on server-side
-        if form.is_valid():
+    # Find substring in each entry in entries:
+    for entry in entries:
+        if term in entry:
+            # Add to results and then display it in results
+            return render(request, "encyclopedia/results.html", {
+        "entries": util.get_entry(term)
+        })
 
-        # Go to page with search term
-        return render(request, "encyclopedia/entry.html", {
-            "entries": util.get_entry(term)
-            })
-    
-    
-    else:
-
-    # Take term as substring
-    # Compare to each entry in list of entries - look at python lesson
-    # Display all relevant entries in results.html 
+        else:
+            return render(request, "encyclopedia/pagenotfound.html")
+             #No page found placeholder
+        # Display all relevant entries in results.html 
            
 
