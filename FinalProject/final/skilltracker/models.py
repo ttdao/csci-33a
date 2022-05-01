4cc2f10d38171d357ddc7330d3bdd432a742ab94
Tien <thientien.dao@gmail.com>
2022-05-01 08:46:10 -0400

    Final

---
+++ FinalProject/final/skilltracker/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+class User(AbstractUser):
+    pass
+
+# Create your models here.
---
+++ FinalProject/final/skilltracker/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    # profile/<str:username>
+    path("profile/<str:username>", views.get_profile, name="profile"),
+    # profile/<str:username>/followingposts
+    path("following_posts", views.follow_posts, name="following"),
+    # editpost
+    # allposts
+    #like/unlike posts
+    path("like/<int:post_id>", views.like, name="like"),
+    #follow/unfollow posts
+    path("follow/<int:post_id>", views.follow, name="like")
+]
